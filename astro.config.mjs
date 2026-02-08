a940843383bcbd092250f121bfed2fe3434a07cc
James Sweeney <jami7shay@Mac.lan>
2026-02-07 20:39:42 -0800

    Add GSC verification, sitemap, and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://zyfqjtntwj-commits.github.io",
+  site: "https://zyfqjtntwj-commits.github.io/shaymentalhealth-site/",
   base: "/shaymentalhealth-site/",
+  integrations: [sitemap()],
 });
+
